cdeed07da3d5d9aa527452b5ffb0cc20dc777d85
lens0021 <lorentz0021@gmail.com>
2022-02-13 11:43:19 +0900

    L10n of two messages

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -7,6 +7,10 @@ export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
 export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
   `The word was ${solution}`
+export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
+  `Must use ${guess} in position ${position}`
+export const NOT_CONTAINED_MESSAGE = (letter: string) =>
+  `Guess must contain ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = 'Statistics'
