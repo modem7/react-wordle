55c8cc76803e8d6e82cd9a8119354838a95740b9
Alireza Ghasemi <ghasemi.a.ir@gmail.com>
2022-02-21 10:01:12 +0100

    Update settings.ts
    Run prettier as per original config

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,8 +1,7 @@
-import { solution } from "../lib/words"
+import { solution } from '../lib/words'
 
 export const MAX_WORD_LENGTH = solution.length
-export const MAX_CHALLENGES = MAX_WORD_LENGTH + 1
+export const MAX_CHALLENGES = MAX_WORD_LENGTH + 3
 export const ALERT_TIME_MS = 2000
 export const REVEAL_TIME_MS = 350
 export const GAME_LOST_INFO_DELAY = (MAX_WORD_LENGTH + 1) * REVEAL_TIME_MS
-export const WELCOME_INFO_MODAL_MS = 350
