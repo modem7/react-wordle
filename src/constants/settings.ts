3d9d642b49c544892119a455bb9e35077fd9d787
Alireza Ghasemi <ghasemi.a.ir@gmail.com>
2022-02-21 09:22:58 +0100

    Update settings.ts
    Set `MAX_WORD_LENGTH` and `MAX_CHALLENGES` dynamically based on the solution of the day, allowing words of varying sizes in different days.

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,5 +1,7 @@
-export const MAX_WORD_LENGTH = 5
-export const MAX_CHALLENGES = 6
+import {solution} from '../lib/words'
+
+export const MAX_WORD_LENGTH = solution.length
+export const MAX_CHALLENGES = MAX_WORD_LENGTH + 1
 export const ALERT_TIME_MS = 2000
 export const REVEAL_TIME_MS = 350
 export const GAME_LOST_INFO_DELAY = (MAX_WORD_LENGTH + 1) * REVEAL_TIME_MS
