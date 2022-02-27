4de0faf1b2c3286585f743cefb6b2506539e36ca
Devin Spikowski <vegeta897@gmail.com>
2022-02-27 15:07:09 -0500

    Lint

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -74,7 +74,7 @@ export const localeAwareUpperCase = (text: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = new Date(2022,0).valueOf()
+  const epochMs = new Date(2022, 0).valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
