3dd929cfd8123cf4ab2cd3314a66597f8378078a
Abrar Hayat <abrarhayat@gmail.com>
2022-03-02 17:44:50 +0600

    Trying to make the histogram style consistent on both ends

--- src/components/stats/Progress.tsx
+++ src/components/stats/Progress.tsx
@@ -8,10 +8,10 @@ export const Progress = ({ index, size, label }: Props) => {
   return (
     <div className="flex justify-left m-1">
       <div className="items-center justify-center w-2">{index + 1}</div>
-      <div className="rounded-full w-full ml-2">
+      <div className="w-full ml-2">
         <div
-          style={{ width: `${5 + size}%` }}
-          className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 rounded-l-full"
+          style={{ width: `${8 + size}%` }}
+          className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5"
         >
           {label}
         </div>
