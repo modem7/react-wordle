e140db51f073a20b0ed0201272e32ca32403bea8
modem7 <modem7@gmail.com>
2022-05-10 17:19:43 +0100

    Converted the modal close div to a button
    This makes the app more accessible and allows keyboard only users to close out of the modal easily.
    
    Copied from
     https://github.com/cwackerfuss/react-wordle/commit/c01597b8bbdfa0a935aa94a7e61536447d5f7ae8

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -47,12 +47,14 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
             <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6 dark:bg-gray-800">
-              <div className="absolute right-4 top-4">
-                <XCircleIcon
-                  className="h-6 w-6 cursor-pointer dark:stroke-white"
-                  onClick={() => handleClose()}
-                />
-              </div>
+              <button
+                onClick={() => handleClose()}
+                tabIndex={0}
+                aria-pressed="false"
+                className="absolute right-4 top-4"
+              >
+                <XCircleIcon className="h-6 w-6 cursor-pointer dark:stroke-white" />
+              </button>
               <div>
                 <div className="text-center">
                   <Dialog.Title
